7d2423d37697323170995d093081acbf299abad1
Mateus Soares <mateussoaress1997@gmail.com>
2020-11-09 22:40:28 -0300

    Removing unnecessary

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+    res.json({ message: "Hello World!" });
 });
 
-router.get('/healthcheck', function(req, res, next) {
-  res.json({ message: 'The server is running (CanvasAPI)' })
+router.get("/healthcheck", function (req, res, next) {
+    res.json({ message: "The server is running (CanvasAPI)" });
 });
 
 module.exports = router;
