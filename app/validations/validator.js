04549b16cdf77ef29fe7a2c72bb450a031e84ed2
Mateus Soares <mateussoaress1997@gmail.com>
2020-11-17 22:33:17 -0300

    Validating and throwing error

--- app/validations/validator.js
+++ app/validations/validator.js
@@ -1,5 +1,8 @@
-const validate = (joiSchema) => {
-    joiSchema.validate();
+const validate = (joiSchema, data) => {
+    const result = joiSchema.validate(data);
+    if (result && result.error) {
+        throw result.error.details[0];
+    }
 };
 
 module.exports = { validate };
--- tests/validator.test.js
+++ tests/validator.test.js
@@ -1,6 +1,13 @@
 const validator = require("../app/validations/validator");
 const Joi = require("joi");
 
+const getJoiSchema = () => {
+    const schema = Joi.object({
+        name: Joi.string().required(),
+    });
+    return schema;
+};
+
 describe("validator:validate", () => {
     it("should be a function", () => {
         expect(validator.validate).toBeInstanceOf(Function);
@@ -9,5 +16,17 @@ describe("validator:validate", () => {
         const mokedSchema = { validate: jest.fn() };
         validator.validate(mokedSchema);
         expect(mokedSchema.validate.mock.calls.length).toBe(1);
+    });
+    it("should throw error when validation fails", () => {
+        const data = {};
+        const mokedSchema = getJoiSchema();
+        expect(function () {
+            validator.validate(mokedSchema, data);
+        }).toThrow({
+            message: '"name" is required',
+            path: ["name"],
+            type: "any.required",
+            context: { label: "name", key: "name" },
+        });
     });
 });
