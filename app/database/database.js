2061bc69a305f6553135de95ebc0cadd3c33734b
Mateus Soares <mateussoaress1997@gmail.com>
2020-11-24 19:35:20 -0300

    database refactoring

--- app/database/database.js
+++ app/database/database.js
@@ -1,23 +1,24 @@
 const mongoose = require("mongoose");
+const { log } = console;
 
 const connect = async () => {
     const host = process.env.DB_HOST;
     const port = process.env.DB_PORT;
-    const dbName = process.env.DB_NAME;
+    const name = process.env.DB_NAME;
     const user = process.env.DB_USER;
     const pass = process.env.DB_PASS;
 
-    const uri = "mongodb://" + host + ":" + port + "/" + dbName;
+    const uri = "mongodb://" + host + ":" + port + "/" + name;
 
     await mongoose.connect(uri, {
         auth: { authSource: "admin" },
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        user: user,
-        pass: pass,
+        user,
+        pass,
     });
 
-    console.log("Connecting to Mongodb with the following config", {
+    log("database -> Connecting to Mongodb with the following config", {
         uri,
         user,
         pass,
@@ -26,7 +27,7 @@ const connect = async () => {
 
 const disconnect = async () => {
     await mongoose.connection.close();
-    console.log("MongoDB was disconnected successfully");
+    log("database -> MongoDB was disconnected successfully");
 };
 
 module.exports = { connect, disconnect };
