debb5f6932cd4e9d3566360d3f37799e4eb86f2e
Mateus Soares <mateussoaress1997@gmail.com>
2020-11-25 21:03:58 -0300

    Changing project structure

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("../app");
+const app = require("../index");
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 
 describe("app", () => {
--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("../app");
+const app = require("../index");
 const { StatusCodes } = require("http-status-codes");
 
 describe("auth/register", () => {
