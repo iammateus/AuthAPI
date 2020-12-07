de1deaef7d8b1c3e7ebb6792f0191738b548025c
Mateus Soares <mateussoaress1997@gmail.com>
2020-12-07 20:49:33 -0300

    Removing old route

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,5 @@ const router = express.Router();
 const { runHealthcheck } = require("../controllers/healthcheck.controller");
 
 router.get("/healthcheck", runHealthcheck);
-router.post("/oi", (req, res, next) => {
-    res.json({ oi: true });
-});
 
 module.exports = router;
