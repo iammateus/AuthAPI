8903cb6f739d3338a4d0bd48b3bcddabebdb111d
Mateus Soares <mateussoaress1997@gmail.com>
2020-12-13 14:23:05 -0300

    getDatabaseUri test

--- tests/databaseConfig.test.js
+++ tests/databaseConfig.test.js
@@ -1,7 +1,17 @@
 const databaseConfig = require("../app/database/databaseConfig");
+const env = require("../app/helpers/env.helper");
 
 describe("databaseConfig:getDatabaseUri", () => {
     it("should be a function", () => {
         expect(databaseConfig.getDatabaseUri).toBeInstanceOf(Function);
+    });
+
+    it("should return mongo connection uri", () => {
+        const host = env("DB_HOST");
+        const port = env("DB_PORT");
+        const name = env("DB_NAME");
+        const uri = "mongodb://" + host + ":" + port + "/" + name;
+        const result = databaseConfig.getDatabaseUri();
+        expect(result).toEqual(uri);
     });
 });
