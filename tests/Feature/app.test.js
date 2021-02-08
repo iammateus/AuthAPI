63e093705caccdd4cfacab77eda47863dfcbbbe6
Mateus Soares <mateussoaress1997@gmail.com>
2021-02-08 19:43:06 -0300

    describe renamed

--- tests/Feature/app.test.js
+++ tests/Feature/app.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../../index");
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 
-describe("app", () => {
+describe("server", () => {
     it("should return a not found response when receiving a request for a non-existent route", async () => {
         const response = await request(app).get("/anything-non-existent");
         expect(response.status).toEqual(StatusCodes.NOT_FOUND);
