11a8e574e3e85120aec95c223f96dfa2bbf8ab4f
Mateus Soares <mateussoaress1997@gmail.com>
2021-03-13 14:23:15 -0300

    Removing Joy details from request responde

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -4,8 +4,10 @@ const { StatusCodes } = require("http-status-codes");
 const validate = async (data, joySchema, res) => {
     try {
         await validator.validate(data, joySchema);
-    } catch (error) {
-        return res.status(StatusCodes.UNPROCESSABLE_ENTITY).json(error);
+    } catch (err) {
+        return res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
+            message: err.message
+        });
     }
 };
 
