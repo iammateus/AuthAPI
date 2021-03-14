b1ac7ea59429bccce895970bea05903d211ccd16
Mateus Soares <mateussoaress1997@gmail.com>
2021-03-14 12:17:16 -0300

    User password hash tests

--- tests/Unit/user.model.test.js
+++ tests/Unit/user.model.test.js
@@ -1,6 +1,6 @@
 const faker = require("faker");
 const User = require("../../app/models/User");
-const database = require("../../app/database/database");
+const hashHelper = require("../../app/helpers/hash.helper")
 const {
     mockDatabaseAndConnect,
     unmockDatabaseAndDisconnect,
@@ -22,8 +22,34 @@ describe("User", () => {
         expect(user.createdAt).toBeTruthy();
         expect(user.updatedAt).toBeTruthy();
     });
+    
+    it("should hash password before saving", async () => {
+        const data = {
+            email: faker.internet.email(),
+            password: faker.lorem.word(8),
+        };
+        const user = new User(data);
+        await user.save();
 
-    //@TODO: tests auto password hashing
+        expect(user.password === data.password).toBe(false)
+        expect(await hashHelper.check(data.password, user.password)).toBe(true)
+    });
+    
+    it("should hash new password before saving", async () => {
+        const data = {
+            email: faker.internet.email(),
+            password: faker.lorem.word(8),
+        };
+        const user = new User(data);
+        await user.save();
+
+        const newPassword = faker.lorem.word(8);
+        user.password = newPassword;
+        await user.save();
+
+        expect(user.password === newPassword).toBe(false)
+        expect(await hashHelper.check(newPassword, user.password)).toBe(true)
+    });
 
     afterAll(async () => {
         await unmockDatabaseAndDisconnect();
